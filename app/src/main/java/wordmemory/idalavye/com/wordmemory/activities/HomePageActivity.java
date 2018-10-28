1b1083a628feff268cd145fd53218bd00b76f6fd
İbrahim Dağdelen <ibrahimdagdelentr@gmail.com>
2018-10-28 12:49:42 +0300

    Initial commit

---
+++ app/src/main/java/wordmemory/idalavye/com/wordmemory/activities/HomePageActivity.java
@@ -0,0 +1,15 @@
+package wordmemory.idalavye.com.wordmemory.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import wordmemory.idalavye.com.wordmemory.R;
+
+public class HomePageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_page);
+    }
+}
