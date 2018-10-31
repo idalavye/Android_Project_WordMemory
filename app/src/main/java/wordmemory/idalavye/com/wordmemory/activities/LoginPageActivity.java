e44677cf41780a3089bef86d9fd7206d3a3a178f
İbrahim Dağdelen <ibrahimdagdelentr@gmail.com>
2018-10-31 14:29:54 +0300

    designed login page screen and changed the application icon

---
+++ app/src/main/java/wordmemory/idalavye/com/wordmemory/activities/LoginPageActivity.java
@@ -0,0 +1,16 @@
+package wordmemory.idalavye.com.wordmemory.activities;
+
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import wordmemory.idalavye.com.wordmemory.R;
+
+public class LoginPageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_page);
+    }
+}
