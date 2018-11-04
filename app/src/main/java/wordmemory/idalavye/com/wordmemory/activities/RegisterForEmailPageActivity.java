9946be777e588b60d50211f74fe76fd28d714d14
LAPTOP-516N2IE7\mvltsevinc <sevincmevlut@gmail.com>
2018-11-04 15:19:44 +0300

    Login Page Init

---
+++ app/src/main/java/wordmemory/idalavye/com/wordmemory/activities/RegisterForEmailPageActivity.java
@@ -0,0 +1,15 @@
+package wordmemory.idalavye.com.wordmemory.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+import wordmemory.idalavye.com.wordmemory.R;
+
+import android.os.Bundle;
+
+public class RegisterForEmailPageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_for_email_page);
+    }
+}
