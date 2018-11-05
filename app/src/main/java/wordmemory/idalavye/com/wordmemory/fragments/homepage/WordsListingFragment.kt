3154df8ba5987cc3fb9096458d33120c01cb21b7
emirhanaydin <emirhanaydin95@gmail.com>
2018-11-05 18:39:13 +0300

    Convert WordsListingFragment to Kotlin and integrate with WordListItemController

---
+++ app/src/main/java/wordmemory/idalavye/com/wordmemory/fragments/homepage/WordsListingFragment.kt
@@ -0,0 +1,39 @@
+package wordmemory.idalavye.com.wordmemory.fragments.homepage
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ExpandableListView
+import androidx.fragment.app.Fragment
+import wordmemory.idalavye.com.wordmemory.R
+import wordmemory.idalavye.com.wordmemory.adapters.ExpandableListViewAdapter
+import wordmemory.idalavye.com.wordmemory.controllers.WordListItemController
+
+class WordsListingFragment : Fragment() {
+    var listView: ExpandableListView? = null
+        private set
+    private var lastExpandedPosition: Int = -1
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val view = inflater.inflate(R.layout.words_listing_fragment, container, false)
+
+        val words = WordListItemController.words ?: return view
+        val activity = activity ?: return view
+        val adapter = ExpandableListViewAdapter(activity, words)
+
+        val listView: ExpandableListView = view.findViewById(R.id.words_listView)
+        listView.isNestedScrollingEnabled = true
+        listView.setAdapter(adapter)
+        listView.setOnGroupExpandListener { groupPosition ->
+            if (lastExpandedPosition >= 0 && lastExpandedPosition != groupPosition) {
+                listView.collapseGroup(lastExpandedPosition)
+            }
+
+            lastExpandedPosition = groupPosition
+        }
+
+        this.listView = listView
+        return view
+    }
+}
