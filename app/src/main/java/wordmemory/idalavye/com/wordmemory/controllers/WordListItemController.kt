87695c45658adb098945683d90b3d4c8670b5d83
İbrahim Dağdelen <ibrahimdagdelentr@gmail.com>
2018-11-09 11:54:37 +0300

    words are now added exclusively to the user

--- app/src/main/java/wordmemory/idalavye/com/wordmemory/controllers/WordListItemController.kt
+++ app/src/main/java/wordmemory/idalavye/com/wordmemory/controllers/WordListItemController.kt
@@ -5,10 +5,11 @@ import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.ValueEventListener
 import wordmemory.idalavye.com.wordmemory.database.DatabaseRef.wordsRef
 import wordmemory.idalavye.com.wordmemory.models.WordListItemModel
+import wordmemory.idalavye.com.wordmemory.utils.Login
 
 object WordListItemController {
     private var wordsMutable: MutableList<WordListItemModel>? = null
-    val words get() = wordsMutable?.toList() as ArrayList?
+    val words get() = wordsMutable as ArrayList?
     private val listeners: MutableList<WordItemDataChangeListener> = mutableListOf()
 
     fun pullWordItems() {
@@ -16,12 +17,19 @@ object WordListItemController {
             override fun onCancelled(p0: DatabaseError) {
                 throw p0.toException()
             }
+
             override fun onDataChange(p0: DataSnapshot) {
                 val words: MutableList<WordListItemModel> = mutableListOf()
+                var userWords: MutableList<WordListItemModel> = mutableListOf()
                 p0.children.mapNotNullTo(words) { it.getValue<WordListItemModel>(WordListItemModel::class.java) }
 
-                this@WordListItemController.wordsMutable = words
+                for (word in words){
+                    if(word._createdAt.equals(Login.getUserId())){
+                        userWords.add(word)
+                    }
+                }
 
+                this@WordListItemController.wordsMutable = userWords
                 for (listener in listeners) {
                     listener.onWordItemDataChange()
                 }
