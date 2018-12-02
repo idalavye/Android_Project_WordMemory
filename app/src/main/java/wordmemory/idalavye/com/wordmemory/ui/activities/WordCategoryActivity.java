9bcf027102a1ff7200c806420b3af363cb0a543c
mvltsevinc <469745mvlt>
2018-12-02 17:17:48 +0300

    wordCategory firebase init

--- app/src/main/java/wordmemory/idalavye/com/wordmemory/ui/activities/WordCategoryActivity.java
+++ app/src/main/java/wordmemory/idalavye/com/wordmemory/ui/activities/WordCategoryActivity.java
@@ -1,17 +1,28 @@
 package wordmemory.idalavye.com.wordmemory.ui.activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import wordmemory.idalavye.com.wordmemory.R;
 import wordmemory.idalavye.com.wordmemory.models.Cards;
 import wordmemory.idalavye.com.wordmemory.ui.adapters.CustomListAdapter;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ListView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 
 public class WordCategoryActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
+
+    FirebaseDatabase database;
+    DatabaseReference myRef;
 
     ListView cardListView;
     @Override
@@ -21,6 +32,9 @@ public class WordCategoryActivity extends AppCompatActivity {
 
         cardListView = findViewById(R.id.cardListView);
 
+        database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("message");
+
         ArrayList<Cards> cardList = new ArrayList<Cards>();
         cardList.add(new Cards("drawable://" + R.drawable.a,"Resim1"));
         cardList.add(new Cards("drawable://" + R.drawable.b,"Resim2"));
@@ -28,6 +42,32 @@ public class WordCategoryActivity extends AppCompatActivity {
         cardList.add(new Cards("drawable://" + R.drawable.b,"Resim2"));
         cardList.add(new Cards("drawable://" + R.drawable.b,"Resim1"));
         cardList.add(new Cards("drawable://" + R.drawable.a,"Resim2"));
+
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                String value = dataSnapshot.getValue(String.class);
+
+
+                Log.d(TAG, "Value is: " + value);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                // Failed to read value
+                Log.w(TAG, "Failed to read value.");
+            }
+        });
+
+     /*   ArrayList<Cards> cardList = new ArrayList<Cards>();
+        cardList.add(new Cards("drawable://" + R.drawable.a,"Resim1"));
+        cardList.add(new Cards("drawable://" + R.drawable.b,"Resim2"));
+        cardList.add(new Cards("drawable://" + R.drawable.a,"Resim1"));
+        cardList.add(new Cards("drawable://" + R.drawable.b,"Resim2"));
+        cardList.add(new Cards("drawable://" + R.drawable.b,"Resim1"));
+        cardList.add(new Cards("drawable://" + R.drawable.a,"Resim2"));*/
 
         CustomListAdapter adapter = new CustomListAdapter(this,R.layout.listview_category,cardList);
         cardListView.setAdapter(adapter);
