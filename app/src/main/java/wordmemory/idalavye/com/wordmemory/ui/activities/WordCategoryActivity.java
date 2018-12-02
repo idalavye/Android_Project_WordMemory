802c33fe176b5585ffe695b3678b597b5864ff12
mvltsevinc <469745mvlt>
2018-12-02 15:55:42 +0300

    WordCategoryActivity init

---
+++ app/src/main/java/wordmemory/idalavye/com/wordmemory/ui/activities/WordCategoryActivity.java
@@ -0,0 +1,15 @@
+package wordmemory.idalavye.com.wordmemory.ui.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+import wordmemory.idalavye.com.wordmemory.R;
+
+import android.os.Bundle;
+
+public class WordCategoryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_word_category);
+    }
+}
