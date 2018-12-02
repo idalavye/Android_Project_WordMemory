8c06113bcf115de53cff46e09e3dbab4ede4444c
mvltsevinc <469745mvlt>
2018-12-02 16:21:47 +0300

    wordcategory activity edited

--- app/src/main/java/wordmemory/idalavye/com/wordmemory/ui/activities/WordCategoryActivity.java
+++ app/src/main/java/wordmemory/idalavye/com/wordmemory/ui/activities/WordCategoryActivity.java
@@ -2,14 +2,35 @@ package wordmemory.idalavye.com.wordmemory.ui.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 import wordmemory.idalavye.com.wordmemory.R;
+import wordmemory.idalavye.com.wordmemory.models.Cards;
+import wordmemory.idalavye.com.wordmemory.ui.adapters.CustomListAdapter;
 
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class WordCategoryActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
 
+    ListView cardListView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_word_category);
+
+        cardListView = findViewById(R.id.cardListView);
+
+        ArrayList<Cards> cardList = new ArrayList<Cards>();
+        cardList.add(new Cards("drawable://" + R.drawable.a,"Resim1"));
+        cardList.add(new Cards("drawable://" + R.drawable.b,"Resim2"));
+        cardList.add(new Cards("drawable://" + R.drawable.a,"Resim1"));
+        cardList.add(new Cards("drawable://" + R.drawable.b,"Resim2"));
+        cardList.add(new Cards("drawable://" + R.drawable.b,"Resim1"));
+        cardList.add(new Cards("drawable://" + R.drawable.a,"Resim2"));
+
+        CustomListAdapter adapter = new CustomListAdapter(this,R.layout.listview_category,cardList);
+        cardListView.setAdapter(adapter);
+
     }
 }
