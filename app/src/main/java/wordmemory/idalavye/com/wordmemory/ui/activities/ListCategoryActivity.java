aa199995005bab4454ffcf2d34a0f90db9359858
mvltsevinc <469745mvlt>
2018-12-08 21:06:03 +0300

    getDatafromFirebase metodu

--- app/src/main/java/wordmemory/idalavye/com/wordmemory/ui/activities/ListCategoryActivity.java
+++ app/src/main/java/wordmemory/idalavye/com/wordmemory/ui/activities/ListCategoryActivity.java
@@ -1,4 +1,50 @@
 package wordmemory.idalavye.com.wordmemory.ui.activities;
 
-public class ListCategoryActivity {
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import wordmemory.idalavye.com.wordmemory.R;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.HashMap;
+
+import static wordmemory.idalavye.com.wordmemory.utils.Login.mAuth;
+
+public class ListCategoryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_category);
+    }
+
+    public  void getDataFromFirebase(){
+
+        String userID = mAuth.getCurrentUser().getUid();
+        DatabaseReference newReference = firebaseDatabase.getReference("records").child(userID);
+
+        newReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for(DataSnapshot ds: dataSnapshot.getChildren()) {
+                    HashMap<String,String> hashMap = (HashMap<String, String>) ds.getValue();
+                    categoryUidFromFB.add(ds.getKey());
+                    categoryTitleFromFB.add(hashMap.get("Title"));
+                    categoryImageFromFB.add(hashMap.get("ImageUrl"));
+                }
+                adapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Toast.makeText(ListCategory.this, "Hata Oluştu", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
 }
