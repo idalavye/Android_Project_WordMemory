879dc91228add8f220549741892fed62e5a24253
mvltsevinc <469745mvlt>
2018-12-08 20:50:22 +0300

    firebase storage

--- app/src/main/java/wordmemory/idalavye/com/wordmemory/ui/activities/CreateCategoryActivity.java
+++ app/src/main/java/wordmemory/idalavye/com/wordmemory/ui/activities/CreateCategoryActivity.java
@@ -1,14 +1,33 @@
 package wordmemory.idalavye.com.wordmemory.ui.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import wordmemory.idalavye.com.wordmemory.R;
 
+import android.net.Uri;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class CreateCategoryActivity extends AppCompatActivity {
+    private static final String TAG = "CreateCategory";
+    ImageView categoryImageView;
+    EditText categoryTitleText;
+
+    FirebaseDatabase firebaseDatabase;
+    DatabaseReference myRef;
+    private FirebaseAuth mAuth;
+    private StorageReference mStorageRef;
+    Uri selectedImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_category);
+
+
     }
 }
