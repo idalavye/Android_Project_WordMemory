d359708922938341c818ddfad6c75e605a57212c
mvltsevinc <469745mvlt>
2018-12-08 20:44:34 +0300

    Category class added

---
+++ app/src/main/java/wordmemory/idalavye/com/wordmemory/models/CategoryClass.java
@@ -0,0 +1,47 @@
+package wordmemory.idalavye.com.wordmemory.models;
+
+import android.app.Activity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.firebase.database.annotations.Nullable;
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+
+import androidx.annotation.NonNull;
+import wordmemory.idalavye.com.wordmemory.R;
+
+public class CategoryClass extends ArrayAdapter<String> {
+    public static ArrayList<String> categoryUid;
+    public static ArrayList<String> categoryTitle;
+    public static ArrayList<String> categoryImage;
+    public static Activity context;
+
+    public CategoryClass(ArrayList<String> categoryUid,ArrayList<String> categoryTitle,ArrayList<String> categoryImage, Activity context) {
+        super(context,R.layout.custom_view,categoryTitle);
+        this.categoryUid = categoryUid;
+        this.categoryTitle = categoryTitle;
+        this.categoryImage = categoryImage;
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        LayoutInflater layoutInflater = context.getLayoutInflater();
+        View customView = layoutInflater.inflate(R.layout.custom_view,null,true);
+
+        TextView categoryText = customView.findViewById(R.id.userEmailTextViewCustomView);
+        ImageView imageView =customView.findViewById(R.id.imageViewCustomView);
+
+        categoryText.setText(categoryTitle.get(position));
+        Picasso.get().load(categoryImage.get(position)).into(imageView);
+
+        return customView;
+    }
+}
