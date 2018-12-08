41b0e62ed7685f873c1bc5c5f043127e88971707
mvltsevinc <469745mvlt>
2018-12-08 20:57:07 +0300

    firebase insert

--- app/src/main/java/wordmemory/idalavye/com/wordmemory/ui/activities/CreateCategoryActivity.java
+++ app/src/main/java/wordmemory/idalavye/com/wordmemory/ui/activities/CreateCategoryActivity.java
@@ -13,19 +13,25 @@ import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.annotations.Nullable;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 import com.squareup.picasso.Picasso;
 
 import java.io.IOException;
+import java.util.UUID;
 
 public class CreateCategoryActivity extends AppCompatActivity {
     private static final String TAG = "CreateCategory";
@@ -63,8 +69,47 @@ public class CreateCategoryActivity extends AppCompatActivity {
 
     }
 
+    public void upload(View view) {
+
+            /*CREATE A NEW CATEGORY*/
+            UUID uuid = UUID.randomUUID();
+            final String imageName = "images/" + uuid +".jpg";
+            StorageReference storageReference = mStorageRef.child(imageName);
+            storageReference.putFile(selectedImage).addOnSuccessListener(this, new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    //download url
+                    StorageReference newReference = FirebaseStorage.getInstance().getReference(imageName);
+                    newReference.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                        @Override
+                        public void onSuccess(Uri uri) {
+                            String downloadURL = uri.toString();
+                            String userID = mAuth.getCurrentUser().getUid();
+                            String title = categoryTitleText.getText().toString();
+
+                            // Dbye kaydet
+                            UUID uuid = UUID.randomUUID();
+                            String uuidStr = uuid.toString();
+                            myRef.child("records").child(userID).child(uuidStr).child("Title").setValue(title);
+                            myRef.child("records").child(userID).child(uuidStr).child("ImageUrl").setValue(downloadURL);
+                            Intent intent = new Intent(getApplicationContext(),ListCategory.class);
+                            startActivity(intent);
+                        }
+                    });
+                }
+            }).addOnFailureListener(this, new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    Toast.makeText(CreateCategory.this, e.getLocalizedMessage(), Toast.LENGTH_SHORT).show();
+                }
+
+            });
+        }
+
+    }
+
     public void catListele(View view){
-        Intent intent = new Intent(getApplicationContext(),ListCategory.class);
+        Intent intent = new Intent(getApplicationContext(),ListCategoryActivity.class);
         startActivity(intent);
     }
 
