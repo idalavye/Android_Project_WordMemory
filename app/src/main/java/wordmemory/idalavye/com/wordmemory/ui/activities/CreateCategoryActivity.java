46cc3b89760d1bfbc019898bcf9a35c75569be6a
mvltsevinc <469745mvlt>
2018-12-08 20:52:27 +0300

    ekran düzenlemeleri

--- app/src/main/java/wordmemory/idalavye/com/wordmemory/ui/activities/CreateCategoryActivity.java
+++ app/src/main/java/wordmemory/idalavye/com/wordmemory/ui/activities/CreateCategoryActivity.java
@@ -3,6 +3,7 @@ package wordmemory.idalavye.com.wordmemory.ui.activities;
 import androidx.appcompat.app.AppCompatActivity;
 import wordmemory.idalavye.com.wordmemory.R;
 
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.widget.EditText;
@@ -11,6 +12,9 @@ import android.widget.ImageView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.squareup.picasso.Picasso;
 
 public class CreateCategoryActivity extends AppCompatActivity {
     private static final String TAG = "CreateCategory";
@@ -27,6 +31,11 @@ public class CreateCategoryActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_category);
+
+        // Get UI elements
+        categoryImageView = findViewById(R.id.categoryImageView);
+        categoryTitleText = findViewById(R.id.categoryTitleText);
+
 
 
     }
