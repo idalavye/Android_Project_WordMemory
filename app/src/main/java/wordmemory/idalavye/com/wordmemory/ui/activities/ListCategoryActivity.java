49a3545af2ef67d7c90b03ae15a773ebb319ee14
mvltsevinc <469745mvlt>
2018-12-08 21:07:55 +0300

    listCat ui elements

--- app/src/main/java/wordmemory/idalavye/com/wordmemory/ui/activities/ListCategoryActivity.java
+++ app/src/main/java/wordmemory/idalavye/com/wordmemory/ui/activities/ListCategoryActivity.java
@@ -3,29 +3,68 @@ package wordmemory.idalavye.com.wordmemory.ui.activities;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import wordmemory.idalavye.com.wordmemory.R;
+import wordmemory.idalavye.com.wordmemory.models.CategoryClass;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ListView;
 import android.widget.Toast;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
 import static wordmemory.idalavye.com.wordmemory.utils.Login.mAuth;
 
 public class ListCategoryActivity extends AppCompatActivity {
+    private static final String TAG = "ListCategory";
+    ListView lstview;
+    CategoryClass adapter;
+    FirebaseDatabase firebaseDatabase;
+    DatabaseReference myRef;
+    ArrayList<String> categoryUidFromFB;
+    ArrayList<String> categoryTitleFromFB;
+    ArrayList<String> categoryImageFromFB;
+    private FirebaseAuth mAuth;
+    private StorageReference mStorageRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_category);
+
+        // Get UI elements
+        lstview = findViewById(R.id.lstview);
+
+        // Initialize ArrayLists
+        categoryUidFromFB = new ArrayList<String>();
+        categoryTitleFromFB = new ArrayList<String>();
+        categoryImageFromFB = new ArrayList<String>();
+        // Database
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        myRef = firebaseDatabase.getReference();
+        //Auth
+        mAuth = FirebaseAuth.getInstance();
+        // Storage
+        mStorageRef = FirebaseStorage.getInstance().getReference();
+
+        // Adapter
+        adapter = new CategoryClass(categoryUidFromFB,categoryTitleFromFB,categoryImageFromFB,this);
+        lstview.setAdapter(adapter);
+        Log.i(TAG, "onCreate: "+ "okey");
+        getDataFromFirebase();
     }
 
     public  void getDataFromFirebase(){
-
+        clearListViewArrays();
         String userID = mAuth.getCurrentUser().getUid();
         DatabaseReference newReference = firebaseDatabase.getReference("records").child(userID);
 
@@ -43,8 +82,15 @@ public class ListCategoryActivity extends AppCompatActivity {
 
             @Override
             public void onCancelled(@NonNull DatabaseError databaseError) {
-                Toast.makeText(ListCategory.this, "Hata Oluştu", Toast.LENGTH_SHORT).show();
+                Toast.makeText(ListCategoryActivity.this, "Hata Oluştu", Toast.LENGTH_SHORT).show();
             }
         });
     }
+
+    private void clearListViewArrays(){
+        categoryUidFromFB.clear();
+        categoryTitleFromFB.clear();
+        categoryImageFromFB.clear();
+    }
+
 }
