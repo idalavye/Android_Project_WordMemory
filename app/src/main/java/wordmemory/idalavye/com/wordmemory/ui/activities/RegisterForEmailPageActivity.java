4a5e5730383886d4d2a52c0f2c95c1cc59c6be63
mvltsevinc <469745mvlt>
2018-12-23 23:27:04 +0300

    Firebase Methods class added

--- app/src/main/java/wordmemory/idalavye/com/wordmemory/ui/activities/RegisterForEmailPageActivity.java
+++ app/src/main/java/wordmemory/idalavye/com/wordmemory/ui/activities/RegisterForEmailPageActivity.java
@@ -4,21 +4,36 @@ import androidx.appcompat.app.AppCompatActivity;
 import wordmemory.idalavye.com.wordmemory.R;
 import wordmemory.idalavye.com.wordmemory.security.EmailLogin;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class RegisterForEmailPageActivity extends AppCompatActivity {
+
+    private Context mContext;
+    private String email,username,password;
+    private EditText mEmail,mUsername,mPassword;
+    private Button btnRegister;
+
+    //Firebase
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+    private FirebaseMethods firebaseMethods;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_for_email_page);
 
-        TextView loginTextView = findViewById(R.id.loginTextView);
-        Button btnRegister = findViewById(R.id.btnRegister);
+        //TextView loginTextView = findViewById(R.id.loginTextView);
+        //Button btnRegister = findViewById(R.id.btnRegister);
 
-        EmailLogin.email_btnRegister(btnRegister,RegisterForEmailPageActivity.this);
-        EmailLogin.email_loginTextView(loginTextView,RegisterForEmailPageActivity.this);
+        //EmailLogin.email_btnRegister(btnRegister,RegisterForEmailPageActivity.this);
+        //EmailLogin.email_loginTextView(loginTextView,RegisterForEmailPageActivity.this);
     }
 }
